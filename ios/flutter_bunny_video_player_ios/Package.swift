4d4d15bd0e03a431a734b7c431d5b59301202705
radixdt2449 <160102368+radixdt2449@users.noreply.github.com>
2026-09-14 18:01:38 +0530

    Update dependency URL for bunny-stream-ios

--- ios/flutter_bunny_video_player_ios/Package.swift
+++ ios/flutter_bunny_video_player_ios/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "flutter-bunny-video-player-ios", targets: ["flutter_bunny_video_player_ios"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RootHex200/bunny-stream-ios.git", revision: "177c6b02baa3956135913ba0a5c8891334c59bc3")
+        .package(url: "https://github.com/radixdt2449/bunny-stream-ios.git", revision: "177c6b02baa3956135913ba0a5c8891334c59bc3")
     ],
     targets: [
         .target(
