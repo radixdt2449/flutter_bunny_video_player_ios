63e47c75a737fe1128dd4898f91be056facedf60
radixdt2449 <160102368+radixdt2449@users.noreply.github.com>
2026-09-15 15:31:27 +0530

    Remove custom video player controls configuration
    Removed custom video player configuration for controls.

--- ios/flutter_bunny_video_player_ios/Sources/flutter_bunny_video_player_ios/BunnyFlutterPlayer.swift
+++ ios/flutter_bunny_video_player_ios/Sources/flutter_bunny_video_player_ios/BunnyFlutterPlayer.swift
@@ -31,11 +31,6 @@ struct BunnyFlutterPlayer: View {
             cacheKey: cacheKey,
             onPlayerReady: onPlayerReady
         )
-        .environment(\.videoPlayerConfig, VideoPlayerConfig(
-            controls: VideoPlayerConfig.Control.allCases.filter {
-                $0 != .fullScreen && $0 != .pip
-            }
-        ))
         .background(Color.clear) // Prevent white flash during video load
     }
 }
